4fcbe5b535a3eacafc5043086ac397b406d8f4e3
Carlos Castro <carloscastrodev@gmail.com>
2022-01-07 23:02:49 -0300

    added equatable to options

---
+++ test/dio_mixin_test.dart
@@ -0,0 +1,15 @@
+import 'package:dio_with_equatable/dio.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('#assureResponse', () {
+    final untypedResponse = Response<dynamic>(
+      requestOptions: RequestOptions(path: ''),
+      data: null,
+    );
+    expect(untypedResponse is Response<int?>, isFalse);
+
+    final typedResponse = DioMixin.assureResponse<int?>(untypedResponse);
+    expect(typedResponse.data, isNull);
+  });
+}
